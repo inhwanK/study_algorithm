76e199267ad84bf36bc97068bfcd6282773b5753
82109 <82109@DESKTOP-J831AQ7>
2021-10-02 21:30:19 +0900

    첫 커밋

---
+++ src/study_algorithm/Bj4344.java
@@ -0,0 +1,14 @@
+package study_algorithm;
+
+import java.util.Scanner;
+
+// 평균은 넘겠지
+// 이번에는 제발...
+// 하
+public class Bj4344 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = 0;
+	}
+}
